70f4273d54e93ea941553b60df5f189460d192e0
Utku Dora <utku.ozdemir@pisano.co>
2016-02-08 00:58:03 -0500

    Random number generation and scoreboard is functional. Background colour changes with score but it can be better in terms of colour adjustment. Possible next step is understanding when game is finished.

--- p02-ozdemir 2048/ViewController.swift
+++ p02-ozdemir 2048/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
    var array: [[Int]] = [[2,2,2,2],[0,2,2,0],[2,2,0,2],[0,0,2,0]]
-    
+    var labelArray :[UILabel]?
+    @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var label00: UILabel!
     @IBOutlet weak var label01: UILabel!
     @IBOutlet weak var label02: UILabel!
@@ -28,7 +29,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var label31: UILabel!
     @IBOutlet weak var label32: UILabel!
     @IBOutlet weak var label33: UILabel!
-   
+    
     
     @IBAction func SwipeLeftAction(sender: AnyObject) {
         for _ in 0...3{ // to slide the numbers to corners and have them move through 0's
@@ -69,10 +70,7 @@ class ViewController: UIViewController {
             }
             
         }
-
-        
-        
-        
+        generateRandomNumbers()
         updateLabels()
     }
 
@@ -123,7 +121,7 @@ class ViewController: UIViewController {
         
         
         
-        
+        generateRandomNumbers()
         
         updateLabels()
 
@@ -185,7 +183,7 @@ class ViewController: UIViewController {
         }
         
         
-        
+        generateRandomNumbers()
         
         updateLabels()
 
@@ -241,36 +239,131 @@ class ViewController: UIViewController {
             
         }
         
-        
-        
-        
+        generateRandomNumbers()
         updateLabels()
         
     }
     
+    
+    func generateRandomNumbers()
+    {
+        for xIndex in 0...3{
+            for yIndex in 0...3  {
+                if array[xIndex][yIndex] == 0 {
+                    let time = UInt32(NSDate().timeIntervalSinceReferenceDate)
+                    srand(time)
+                    
+                    if arc4random()%10 > 7  {
+                        
+                        array[xIndex][yIndex] = 2
+                        
+                    }
+                    
+                }
+                
+                
+            }
+        }
+        
+        
+    }
+    
+    func setBackgroundColor(number:Int)-> UIColor{
+    
+        switch number{
+        case 0:
+            return UIColor.lightGrayColor()
+        case 2:
+            return UIColor.greenColor()
+        case 4:
+            return UIColor.blueColor()
+        case 8:
+            return UIColor.purpleColor()
+        case 16:
+            return UIColor.orangeColor()
+        case 32:
+            return UIColor.yellowColor()
+        case 64:
+            return UIColor.redColor()
+        default:
+            return UIColor.redColor()
+        }
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
     func updateLabels(){
-        label00.text = "\(array[0][0])"
-        label01.text = "\(array[0][1])"
-        label02.text = "\(array[0][2])"
-        label03.text = "\(array[0][3])"
-        label10.text = "\(array[1][0])"
-        label11.text = "\(array[1][1])"
-        label12.text = "\(array[1][2])"
-        label13.text = "\(array[1][3])"
-        label20.text = "\(array[2][0])"
-        label21.text = "\(array[2][1])"
-        label22.text = "\(array[2][2])"
-        label23.text = "\(array[2][3])"
-        label30.text = "\(array[3][0])"
-        label31.text = "\(array[3][1])"
-        label32.text = "\(array[3][2])"
-        label33.text = "\(array[3][3])"
+     
+        
+        let maxElement = array.map({ $0.maxElement()!}).maxElement()!
+        scoreLabel.text = "\(maxElement)"
+        scoreLabel.backgroundColor = setBackgroundColor(maxElement)
+        var count = 0
+        for xIndex in 0...3{
+            for yIndex in 0...3  {
+                labelArray![count].text = "\(array[xIndex][yIndex])"
+                labelArray![count].backgroundColor = setBackgroundColor(array[xIndex][yIndex])
+                count++
+            }
+        }
+
+        
+        
+        
+        
+//        label00.text = "\(array[0][0])"
+//        label01.text = "\(array[0][1])"
+//        label02.text = "\(array[0][2])"
+//        label03.text = "\(array[0][3])"
+//        label10.text = "\(array[1][0])"
+//        label11.text = "\(array[1][1])"
+//        label12.text = "\(array[1][2])"
+//        label13.text = "\(array[1][3])"
+//        label20.text = "\(array[2][0])"
+//        label21.text = "\(array[2][1])"
+//        label22.text = "\(array[2][2])"
+//        label23.text = "\(array[2][3])"
+//        label30.text = "\(array[3][0])"
+//        label31.text = "\(array[3][1])"
+//        label32.text = "\(array[3][2])"
+//        label33.text = "\(array[3][3])"
+//        
+//        label00.backgroundColor = setBackgroundColor(array[0][0])
+//        label01.backgroundColor = setBackgroundColor(array[0][1])
+//        label02.backgroundColor = setBackgroundColor(array[0][2])
+//        label03.backgroundColor = setBackgroundColor(array[0][3])
+//        label10.backgroundColor = setBackgroundColor(array[1][0])
+//        label11.backgroundColor = setBackgroundColor(array[1][1])
+//        label12.backgroundColor = setBackgroundColor(array[1][2])
+//        label13.backgroundColor = setBackgroundColor(array[1][3])
+//        label20.backgroundColor = setBackgroundColor(array[2][0])
+//        label21.backgroundColor = setBackgroundColor(array[2][1])
+//        label22.backgroundColor = setBackgroundColor(array[2][2])
+//        label23.backgroundColor = setBackgroundColor(array[2][3])
+//        label30.backgroundColor = setBackgroundColor(array[3][0])
+//        label31.backgroundColor = setBackgroundColor(array[3][1])
+//        label32.backgroundColor = setBackgroundColor(array[3][2])
+//        label33.backgroundColor = setBackgroundColor(array[3][3])
+        
+
     
     
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+         labelArray = [label00,label01,label02,label03,label10,label11,label12,label13,label20,label21,label22,label23,label30,label31,label32,label33, ]
         updateLabels()
+        
+        
 //        for xIndex in 0...3{
 //            for yIndex in 0...3 {
 //            
